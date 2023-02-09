58aa3b4dd95ed855a7ea7ea2d267fcc8354f94ce
LayneHaber <layne.haber@gmail.com>
2023-02-09 14:09:21 -0700

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "dotenv/config";
+import { config as envConfig } from "dotenv";
+import "@nomiclabs/hardhat-etherscan";
+
+envConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -8,9 +11,14 @@ const config: HardhatUserConfig = {
     optimism: {
       url: "https://mainnet.optimism.io",
       // PRIVATE_KEY loaded from .env file
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY!,
+  },
 };
 
 export default config;
