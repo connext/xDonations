84cf00ca0bd4d20573d9fde358e32fdb99d34103
arjunbhuptani <arjunbhuptani@gmail.com>
2023-02-09 13:28:27 +0000

    Fix deployment script

--- contracts/xDonate.sol
+++ contracts/xDonate.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 import {IConnext} from "@connext/smart-contracts/contracts/core/connext/interfaces/IConnext.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    optimism: {
+      url: "https://mainnet.optimism.io",
+      // PRIVATE_KEY loaded from .env file
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
