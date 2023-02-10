eb8a50a3d702b8837a1dc7f36e55bd1e9043e48a
Layne Haber <layne.haber@gmail.com>
2023-02-10 12:54:02 -0700

    config: update defaults to proper values; remove polygon

--- deploy/00_deploy.ts
+++ deploy/00_deploy.ts
@@ -1,5 +1,5 @@
-import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {DeployFunction} from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
 import { config as envConfig } from "dotenv";
 
 envConfig();
@@ -15,15 +15,15 @@ export const DEFAULT_ARGS: Record<number, string[]> = {
     10: [
         "0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA", // Connext on Optimism
         "0x4200000000000000000000000000000000000006", // Weth on Optimism
-        "0xf7f0CFC3772d29d4CC1482A2ACB7Be16a85a2223", // My address Ethereum
+        "0xe1935271D1993434A1a59fE08f24891Dc5F398Cd", // Donation address Ethereum
         "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", // USDC on Optimism (donation asset)
         "6648936" // Ethereum domain ID
     ],
-    137: [
-        "0x11984dc4465481512eb5b777E44061C158CF2259", // Connext on Polygon
-        "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", // Weth on Polygon
-        "0xf7f0CFC3772d29d4CC1482A2ACB7Be16a85a2223", // My address Ethereum
-        "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC on Polygon (donation asset)
+    42161: [
+        "0xEE9deC2712cCE65174B561151701Bf54b99C24C8", // Connext on Arbitrum
+        "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", // Weth on Arbitrum
+        "0xe1935271D1993434A1a59fE08f24891Dc5F398Cd", // Donation address Ethereum
+        "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", // USDC on Arbitrum (donation asset)
         "6648936" // Ethereum domain ID
     ],
 }
